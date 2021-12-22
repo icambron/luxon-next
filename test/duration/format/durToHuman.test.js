2c1fa41e1233e271d4ff1f3a89a3e3bfea870b0e
Isaac Cambron <isaac@isaaccambron.com>
2021-12-21 22:10:26 -0500

    add test for intl humanize

--- test/duration/format/durToHuman.test.js
+++ test/duration/format/durToHuman.test.js
@@ -22,3 +22,7 @@ test("durToHuman accepts a listStyle", () => {
 test("durToHuman accepts number format opts", () => {
   expect(durToHuman(dur, { unitDisplay: "short" })).toEqual("1 yr, 2 mths, 1 wk, 3 days, 4 hr, 5 min, 6 sec, 7 ms");
 });
+
+test("Duration#toHuman works in differt languages", () => {
+  expect(durToHuman(dur, "fr")).toEqual("1 an, 2 mois, 1 semaine, 3 jours, 4 heures, 5 minutes, 6 secondes, 7 millisecondes");
+});
